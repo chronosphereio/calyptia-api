0dc3ab2e7273411ca83156beded7594d98e05242
Nicolás Parada <parada.nicolas@outlook.com>
2022-01-21 00:02:48 -0300

    client: test tokens to 66% coverage

---
+++ client/token_test.go
@@ -0,0 +1,92 @@
+package client_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/calyptia/api/types"
+)
+
+func TestClient_CreateToken(t *testing.T) {
+	ctx := context.Background()
+
+	asUser := userClient(t)
+	project := defaultProject(t, asUser)
+
+	got, err := asUser.CreateToken(ctx, project.ID, types.CreateToken{
+		Name: "test-token",
+	})
+	wantEqual(t, err, nil)
+	wantNoEqual(t, got.ID, "")
+	wantEqual(t, got.Name, "test-token")
+	wantNoEqual(t, got.Token, "")
+	wantNoTimeZero(t, got.CreatedAt)
+}
+
+func TestClient_Tokens(t *testing.T) {
+	ctx := context.Background()
+
+	asUser := userClient(t)
+	project := defaultProject(t, asUser)
+
+	token, err := asUser.CreateToken(ctx, project.ID, types.CreateToken{
+		Name: "test-token",
+	})
+	wantEqual(t, err, nil)
+
+	got, err := asUser.Tokens(ctx, project.ID, types.TokensParams{})
+	wantEqual(t, err, nil)
+
+	wantEqual(t, len(got), 2) // Aditional "default" token should be created by default with each project.
+
+	wantEqual(t, got[0], token)
+	wantEqual(t, got[1].Name, "default")
+}
+
+func TestClient_Token(t *testing.T) {
+	ctx := context.Background()
+
+	asUser := userClient(t)
+	project := defaultProject(t, asUser)
+
+	token, err := asUser.CreateToken(ctx, project.ID, types.CreateToken{
+		Name: "test-token",
+	})
+	wantEqual(t, err, nil)
+
+	got, err := asUser.Token(ctx, token.ID)
+	wantEqual(t, err, nil)
+	wantEqual(t, got, token)
+}
+
+func TestClient_UpdateToken(t *testing.T) {
+	ctx := context.Background()
+
+	asUser := userClient(t)
+	project := defaultProject(t, asUser)
+
+	token, err := asUser.CreateToken(ctx, project.ID, types.CreateToken{
+		Name: "test-token",
+	})
+	wantEqual(t, err, nil)
+
+	err = asUser.UpdateToken(ctx, token.ID, types.UpdateToken{
+		Name: ptrStr("test-token-updated"),
+	})
+	wantEqual(t, err, nil)
+}
+
+func TestClient_DeleteToken(t *testing.T) {
+	ctx := context.Background()
+
+	asUser := userClient(t)
+	project := defaultProject(t, asUser)
+
+	token, err := asUser.CreateToken(ctx, project.ID, types.CreateToken{
+		Name: "test-token",
+	})
+	wantEqual(t, err, nil)
+
+	err = asUser.DeleteToken(ctx, token.ID)
+	wantEqual(t, err, nil)
+}
