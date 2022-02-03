9cc1d1f9e91e862abafeb60da727c86ba29738e3
Nicolás Parada <parada.nicolas@outlook.com>
2022-02-03 13:33:44 -0300

    types: remove invitation model

--- types/invitation.go
+++ types/invitation.go
@@ -1,16 +1,5 @@
 package types
 
-import (
-	"time"
-)
-
-// Invitation model.
-type Invitation struct {
-	ID        string    `json:"id" yaml:"id"`
-	Email     string    `json:"email" yaml:"email"`
-	CreatedAt time.Time `json:"createdAt" yaml:"createdAt"`
-}
-
 // CreateInvitation request payload for creating a project invitation.
 type CreateInvitation struct {
 	Email       string `json:"email"`
