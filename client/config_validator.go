a7e8a71d3c58732472b44598632d9044b183783b
Jorge Niedbalski <jnr@metaklass.org>
2022-02-10 13:46:58 +0100

    spec/client for config validation v2.
    * Adds spec definition for v2
    * Adds client method for calling config_validate_v2
    
    Signed-off-by: Jorge Niedbalski <j@calyptia.com>

--- client/config_validator.go
+++ client/config_validator.go
@@ -14,3 +14,10 @@ func (c *Client) ValidateConfig(ctx context.Context, agentType types.AgentType, 
 	var out types.ValidatedConfig
 	return out, c.do(ctx, http.MethodPost, "/v1/config_validate/"+url.PathEscape(string(agentType)), payload, &out)
 }
+
+// ValidateConfigV2 validates that an already parsed fluentbit(only) config to check if semantically valid
+// To parse the raw agent config take a look at https://github.com/calyptia/fluent-bit-config-parser.
+func (c *Client) ValidateConfigV2(ctx context.Context, payload types.ValidatingConfig) (types.ValidatedConfigV2, error) {
+	var out types.ValidatedConfigV2
+	return out, c.do(ctx, http.MethodPost, "/v1/config_validate_v2", payload, &out)
+}
