bf2d081b19b74ec8477bcdedad40cc2c39bdb9cd
Jorge Niedbalski <j@calyptia.com>
2022-02-10 14:54:52 +0100

    spec/client for config validation v2. (#30)
    * Adds spec definition for v2
    * Adds client method for calling config_validate_v2
    
    Signed-off-by: Jorge Niedbalski <j@calyptia.com>

--- client/config_validator_test.go
+++ client/config_validator_test.go
@@ -39,3 +39,37 @@ func TestClient_ValidateConfig(t *testing.T) {
 	wantEqual(t, len(got.Errors.Filter), 0)
 	wantEqual(t, len(got.Errors.Output), 0)
 }
+
+func TestClient_ValidateConfigV2(t *testing.T) {
+	if testFluentbitConfigValidatorAPIKey == "" || testFluentdConfigValidatorAPIKey == "" {
+		t.Skip("TODO: setup fluentbit/d config validator API keys")
+	}
+
+	ctx := context.Background()
+	asUser := userClient(t)
+	withToken := withToken(t, asUser)
+
+	// Yes, config validation requires project token auth.
+	got, err := withToken.ValidateConfigV2(ctx, types.ValidatingConfig{
+		Configs: []types.ValidatingConfigEntry{
+			{
+				Command: "INPUT",
+				Name:    "tail",
+			},
+			{
+				Command: "FILTER",
+				Name:    "record_modifier",
+			},
+			{
+				Command: "OUTPUT",
+				Name:    "stdout",
+			},
+		},
+	})
+
+	wantEqual(t, err, nil)
+	wantEqual(t, len(got.Errors.Runtime), 0)
+	wantEqual(t, len(got.Errors.Input), 0)
+	wantEqual(t, len(got.Errors.Filter), 0)
+	wantEqual(t, len(got.Errors.Output), 0)
+}
--- types/config_validator.go
+++ types/config_validator.go
@@ -25,3 +25,30 @@ type ConfigValidity struct {
 	Output  map[string][]string `json:"output"`
 	Filter  map[string][]string `json:"filter"`
 }
+
+// ConfigValidityV2Property property details.
+type ConfigValidityV2Property struct {
+	ID       string   `json:"id"`
+	Property string   `json:"property"`
+	Text     string   `json:"text"`
+	Errors   []string `json:"errors"`
+}
+
+// ConfigValidityV2Runtime runtime details.
+type ConfigValidityV2Runtime struct {
+	ID     string   `json:"id"`
+	Errors []string `json:"errors"`
+}
+
+// ConfigValidityV2 details.
+type ConfigValidityV2 struct {
+	Runtime []ConfigValidityV2Runtime             `json:"runtime" `
+	Input   map[string][]ConfigValidityV2Property `json:"input"`
+	Output  map[string][]ConfigValidityV2Property `json:"output"`
+	Filter  map[string][]ConfigValidityV2Property `json:"filter"`
+}
+
+// ValidatedConfigV2 response body after validating an agent config successfully against the v2 endpoint.
+type ValidatedConfigV2 struct {
+	Errors ConfigValidityV2 `json:"errors"`
+}
