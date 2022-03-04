516e938b453985ddf005e6e87eb9bdff5054cb76
Jorge Niedbalski <j@calyptia.com>
2022-03-04 16:22:48 +0100

    aggregator: add version field.
    * Add version field to aggregator.

--- types/aggregator.go
+++ types/aggregator.go
@@ -2,11 +2,16 @@ package types
 
 import "time"
 
+const (
+	DefaultAggregatorVersion = "v0.1.12"
+)
+
 // Aggregator model.
 type Aggregator struct {
 	ID             string    `json:"id" yaml:"id"`
 	Token          string    `json:"token" yaml:"token"`
 	Name           string    `json:"name" yaml:"name"`
+	Version        string    `json:"version" yaml:"version"`
 	PipelinesCount uint64    `json:"pipelinesCount" yaml:"pipelinesCount"`
 	CreatedAt      time.Time `json:"createdAt" yaml:"createdAt"`
 	UpdatedAt      time.Time `json:"updatedAt" yaml:"updatedAt"`
@@ -15,18 +20,21 @@ type Aggregator struct {
 // CreateAggregator request payload for creating a new aggregator.
 type CreateAggregator struct {
 	Name                    string `json:"name"`
+	Version                 string `json:"version" yaml:"version"`
 	AddHealthCheckPipeline  bool   `json:"addHealthCheckPipeline"`
 	HealthCheckPipelinePort uint   `json:"healthCheckPipelinePort"`
 }
 
 // CreatedAggregator response payload after creating an aggregator successfully.
 type CreatedAggregator struct {
-	ID            string    `json:"id"`
-	Token         string    `json:"token"`
-	PrivateRSAKey []byte    `json:"privateRSAKey"`
-	PublicRSAKey  []byte    `json:"publicRSAKey"`
-	Name          string    `json:"name"`
-	CreatedAt     time.Time `json:"createdAt"`
+	ID            string `json:"id"`
+	Token         string `json:"token"`
+	PrivateRSAKey []byte `json:"privateRSAKey"`
+	PublicRSAKey  []byte `json:"publicRSAKey"`
+	Name          string `json:"name"`
+	Version       string `json:"version"`
+
+	CreatedAt time.Time `json:"createdAt"`
 
 	HealthCheckPipeline *Pipeline         `json:"healthCheckPipeline"`
 	ResourceProfiles    []ResourceProfile `json:"resourceProfiles"`
@@ -40,5 +48,6 @@ type AggregatorsParams struct {
 
 // UpdateAggregator request payload for updating an aggregator.
 type UpdateAggregator struct {
-	Name *string `json:"name"`
+	Name    *string `json:"name"`
+	Version *string `json:"version"`
 }
