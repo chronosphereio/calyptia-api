65cea7057d7050509adb187bae0d81dc52cf4f0c
Jorge Niedbalski <jnr@metaklass.org>
2022-03-05 17:49:47 +0100

    update version tests
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- client/aggregator_test.go
+++ client/aggregator_test.go
@@ -15,9 +15,11 @@ func TestClient_CreateAggregator(t *testing.T) {
 
 	got, err := withToken.CreateAggregator(ctx, types.CreateAggregator{
 		Name:                   "test-aggregator",
+		Version:                types.DefaultAggregatorVersion,
 		AddHealthCheckPipeline: true,
 	})
 	wantEqual(t, err, nil)
+	wantEqual(t, got.Version, types.DefaultAggregatorVersion)
 	wantEqual(t, got.Name, "test-aggregator")
 	wantNoEqual(t, got.Token, "")
 	wantNoEqual(t, got.PrivateRSAKey, "")
@@ -40,10 +42,12 @@ func TestClient_Aggregators(t *testing.T) {
 
 	project := defaultProject(t, asUser)
 	got, err := asUser.Aggregators(ctx, project.ID, types.AggregatorsParams{})
+
 	wantEqual(t, err, nil)
 	wantEqual(t, len(got.Items), 1)
 	wantEqual(t, got.Items[0].ID, created.ID)
 	wantEqual(t, got.Items[0].Name, created.Name)
+	wantEqual(t, got.Items[0].Version, created.Version)
 	wantEqual(t, got.Items[0].Token, created.Token)
 	wantEqual(t, got.Items[0].PipelinesCount, uint64(1))
 	wantEqual(t, got.Items[0].CreatedAt, created.CreatedAt)
@@ -65,6 +69,7 @@ func TestClient_Aggregator(t *testing.T) {
 	wantEqual(t, err, nil)
 	wantEqual(t, got.ID, created.ID)
 	wantEqual(t, got.Name, created.Name)
+	wantEqual(t, got.Version, created.Version)
 	wantEqual(t, got.Token, created.Token)
 	wantEqual(t, got.PipelinesCount, uint64(1))
 	wantEqual(t, got.CreatedAt, created.CreatedAt)
@@ -86,6 +91,26 @@ func TestClient_UpdateAggregator(t *testing.T) {
 		Name: ptrStr("test-aggregator-updated"),
 	})
 	wantEqual(t, err, nil)
+
+	t.Run("version", func(t *testing.T) {
+		err = withToken.UpdateAggregator(ctx, created.ID, types.UpdateAggregator{
+			Version: ptrStr(types.DefaultAggregatorVersion),
+		})
+
+		wantEqual(t, err, nil)
+
+		err = withToken.UpdateAggregator(ctx, created.ID, types.UpdateAggregator{
+			Version: ptrStr("non-semver-version"),
+		})
+
+		wantNoEqual(t, err, nil)
+
+		err = withToken.UpdateAggregator(ctx, created.ID, types.UpdateAggregator{
+			Version: ptrStr(""),
+		})
+
+		wantNoEqual(t, err, nil)
+	})
 }
 
 func TestClient_DeleteAggregator(t *testing.T) {
@@ -108,7 +133,8 @@ func setupAggregator(t *testing.T, withToken *client.Client) types.CreatedAggreg
 	ctx := context.Background()
 
 	aggregator, err := withToken.CreateAggregator(ctx, types.CreateAggregator{
-		Name: "test-aggregator",
+		Name:    "test-aggregator",
+		Version: types.DefaultAggregatorVersion,
 	})
 	wantEqual(t, err, nil)
 
