d14aa86b1ce5d23b8e189bc362c4f1b03168e968
Gabriel Bussolo <gabriel@calyptia.com>
2022-03-23 07:55:15 +0300

    Add pagination functionality to tokens

--- client/token_test.go
+++ client/token_test.go
@@ -2,6 +2,7 @@ package client_test
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	"github.com/calyptia/api/types"
@@ -29,18 +30,38 @@ func TestClient_Tokens(t *testing.T) {
 	asUser := userClient(t)
 	project := defaultProject(t, asUser)
 
-	token, err := asUser.CreateToken(ctx, project.ID, types.CreateToken{
-		Name: "test-token",
+	t.Run("ok", func(t *testing.T) {
+		token, err := asUser.CreateToken(ctx, project.ID, types.CreateToken{
+			Name: "test-token",
+		})
+		wantEqual(t, err, nil)
+
+		got, err := asUser.Tokens(ctx, project.ID, types.TokensParams{})
+		wantEqual(t, err, nil)
+
+		wantEqual(t, len(got.Items), 2) // Aditional "default" token should be created by default with each project.
+
+		wantEqual(t, got.Items[0], token)
+		wantEqual(t, got.Items[1].Name, "default")
 	})
-	wantEqual(t, err, nil)
+	t.Run("pagination", func(t *testing.T) {
+		for i := 0; i < 9; i++ {
+			_, err := asUser.CreateToken(ctx, project.ID, types.CreateToken{
+				Name: "test-token" + strconv.Itoa(i),
+			})
+			wantEqual(t, err, nil)
+		}
 
-	got, err := asUser.Tokens(ctx, project.ID, types.TokensParams{})
-	wantEqual(t, err, nil)
+		allTokens, err := asUser.Tokens(ctx, project.ID, types.TokensParams{})
+		wantEqual(t, err, nil)
+		page1, err := asUser.Tokens(ctx, project.ID, types.TokensParams{Last: ptrUint64(3)})
+		wantEqual(t, err, nil)
+		page2, err := asUser.Tokens(ctx, project.ID, types.TokensParams{Last: ptrUint64(3), Before: page1.EndCursor})
+		wantEqual(t, err, nil)
 
-	wantEqual(t, len(got.Items), 2) // Aditional "default" token should be created by default with each project.
-
-	wantEqual(t, got.Items[0], token)
-	wantEqual(t, got.Items[1].Name, "default")
+		want := allTokens.Items[3:6]
+		wantEqual(t, page2.Items, want)
+	})
 }
 
 func TestClient_Token(t *testing.T) {
