08d2f4ad2d0200732dcac9a0a7b2ed3304e9e0aa
Gabriel Bussolo <gabreeiel@gmail.com>
2022-05-20 12:10:06 +0300

    Create API test for environments

---
+++ client/environment.go
@@ -0,0 +1,47 @@
+package client
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/calyptia/api/types"
+)
+
+// CreateEnvironment within a project.
+func (c *Client) CreateEnvironment(ctx context.Context, projectID string, payload types.CreateEnvironment) (types.CreatedEnvironment, error) {
+	var out types.CreatedEnvironment
+	return out, c.do(ctx, http.MethodPost, "/v1/projects/"+url.PathEscape(projectID)+"/environments", payload, &out)
+}
+
+// Environments from the given project in descending order.
+func (c *Client) Environments(ctx context.Context, projectID string, params types.EnvironmentsParams) (types.Environments, error) {
+	q := url.Values{}
+	if params.Last != nil {
+		q.Set("last", strconv.FormatUint(*params.Last, uintBase))
+	}
+	if params.Before != nil {
+		q.Set("before", *params.Before)
+	}
+
+	var out types.Environments
+	path := "/v1/projects/" + url.PathEscape(projectID) + "/environments?" + q.Encode()
+	return out, c.do(ctx, http.MethodGet, path, nil, &out.Items, withCursor(&out.EndCursor))
+}
+
+// Environment by ID.
+func (c *Client) Environment(ctx context.Context, projectID, environmentID string) (types.Environment, error) {
+	var out types.Environment
+	return out, c.do(ctx, http.MethodGet, "/v1/projects/"+url.PathEscape(projectID)+"/environments/"+url.PathEscape(environmentID), nil, &out)
+}
+
+// UpdateEnvironment by its ID.
+func (c *Client) UpdateEnvironment(ctx context.Context, projectID, environmentID string, payload types.UpdateEnvironment) error {
+	return c.do(ctx, http.MethodPatch, "/v1/projects/"+url.PathEscape(projectID)+"/environments/"+url.PathEscape(environmentID), payload, nil)
+}
+
+// DeleteEnvironment by its ID.
+func (c *Client) DeleteEnvironment(ctx context.Context, projectID, environmentID string) error {
+	return c.do(ctx, http.MethodDelete, "/v1/projects/"+url.PathEscape(projectID)+"/environments/"+url.PathEscape(environmentID), nil, nil)
+}
