efb0602ca429642acd408a8f0275bb5f2cd75de1
Nicolás Parada <parada.nicolas@outlook.com>
2022-08-01 17:00:17 -0400

    json duration

--- types/trace.go
+++ types/trace.go
@@ -2,28 +2,29 @@ package types
 
 import (
 	"encoding/json"
+	"errors"
 	"time"
 )
 
 // TraceSession model.
 type TraceSession struct {
-	ID         string        `json:"id" yaml:"id"`
-	PipelineID string        `json:"pipelineID" yaml:"pipelineID"`
-	Plugins    []string      `json:"plugins" yaml:"plugins"`
-	Lifespan   time.Duration `json:"lifespan" yaml:"lifespan"`
-	CreatedAt  time.Time     `json:"createdAt" yaml:"createdAt"`
-	UpdatedAt  time.Time     `json:"updatedAt" yaml:"updatedAt"`
+	ID         string    `json:"id" yaml:"id"`
+	PipelineID string    `json:"pipelineID" yaml:"pipelineID"`
+	Plugins    []string  `json:"plugins" yaml:"plugins"`
+	Lifespan   Duration  `json:"lifespan" yaml:"lifespan"`
+	CreatedAt  time.Time `json:"createdAt" yaml:"createdAt"`
+	UpdatedAt  time.Time `json:"updatedAt" yaml:"updatedAt"`
 }
 
 // Active tells whether a session is still within its lifespan.
 func (ts TraceSession) Active() bool {
-	return ts.CreatedAt.Add(ts.Lifespan).After(time.Now())
+	return ts.CreatedAt.Add(time.Duration(ts.Lifespan)).After(time.Now())
 }
 
 // CreateTraceSession request payload for creating a new trace session.
 type CreateTraceSession struct {
-	Plugins  []string      `json:"plugins"`
-	Lifespan time.Duration `json:"lifespan"`
+	Plugins  []string `json:"plugins"`
+	Lifespan Duration `json:"lifespan"`
 }
 
 // TraceSessionsParams request payload for querying trace sessions.
@@ -40,8 +41,8 @@ type TraceSessions struct {
 
 // UpdateTraceSession request payload for updating a trace session.
 type UpdateTraceSession struct {
-	Plugins  *[]string      `json:"plugins"`
-	Lifespan *time.Duration `json:"lifespan"`
+	Plugins  *[]string `json:"plugins"`
+	Lifespan *Duration `json:"lifespan"`
 }
 
 // TraceRecord model.
@@ -97,4 +98,35 @@ type TraceRecordsParams struct {
 type TraceRecords struct {
 	Items     []TraceRecord `json:"items"`
 	EndCursor *string       `json:"endCursor"`
+}
+
+// Duration is a time.Duration wrapper that adds support for encoding/json.
+type Duration time.Duration
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Duration(d).String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		*d = Duration(time.Duration(value))
+		return nil
+	case int64:
+		*d = Duration(time.Duration(value))
+		return nil
+	case string:
+		tmp, err := time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		*d = Duration(tmp)
+		return nil
+	default:
+		return errors.New("invalid duration")
+	}
 }
