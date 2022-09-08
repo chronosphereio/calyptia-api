01b69aa357958449be45bf0aa7dc1578a8fe872a
Phillip Whelan <phil@calyptia.com>
2022-09-08 12:20:27 -0400

    pipeline: updated event fields to match code conventions.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- types/pipeline_events.go
+++ types/pipeline_events.go
@@ -11,12 +11,12 @@ const (
 
 // PipelineEvent model.
 type PipelineEvent struct {
-	PipelineID string              `json:"pipeline_id" yaml:"pipeline_id"`
+	PipelineID string              `json:"pipelineID" yaml:"pipelineID"`
 	System     PipelineEventSystem `json:"system" yaml:"system"`
 	Status     PipelineStatusKind  `json:"kind" yaml:"kind"`
 	Reason     string              `json:"reason" yaml:"reason"`
 	Message    string              `json:"message" yaml:"message"`
-	CreatedAt  time.Time           `json:"created_at" yaml:"created_at"`
+	CreatedAt  time.Time           `json:"createdAt" yaml:"createdAt"`
 }
 
 // CreatePipelineEvents is the format we submit the events in.
