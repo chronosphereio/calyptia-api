656c4ed171e037e19195f792a3c96191d6b7130a
Phillip Whelan <phil@calyptia.com>
2022-09-12 11:38:32 -0300

    pipeline/events: update URL.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- client/pipeline_events.go
+++ client/pipeline_events.go
@@ -12,7 +12,7 @@ import (
 // The pipeline name must be unique within the aggregator.
 // The resource profile must exist already. If you don't provide one, it will default to "best-effort-low-resource".
 // Use them to easily deploy configured agents to the aggregator.
-func (c *Client) CreatePipelineEvents(ctx context.Context, aggregagotID string, payload types.CreatePipelineEvent) (types.CreatedPipelineEvent, error) {
+func (c *Client) CreatePipelineEvents(ctx context.Context, pipelineID string, payload types.CreatePipelineEvent) (types.CreatedPipelineEvent, error) {
 	var out types.CreatedPipelineEvent
-	return out, c.do(ctx, http.MethodPost, "/v1/aggregators/"+url.PathEscape(aggregagotID)+"/pipeline_event", payload, &out)
+	return out, c.do(ctx, http.MethodPost, "/v1/aggregator_pipelines/"+url.PathEscape(pipelineID)+"/event", payload, &out)
 }
