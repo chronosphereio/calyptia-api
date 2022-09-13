0d8d9bc5e94d5425294d1e266c378c01044c98be
Phillip Whelan <phil@calyptia.com>
2022-09-13 09:59:26 -0300

    pipeline/events: add events to status.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

--- types/pipeline_status.go
+++ types/pipeline_status.go
@@ -2,11 +2,27 @@ package types
 
 import "time"
 
+type PipelineEventSystem string
+
+const (
+	PipelineEventSystemDeployment PipelineEventSystem = "k8s:deployment"
+	PipelineEventSystemPod        PipelineEventSystem = "k8s:pod"
+)
+
+// PipelineEvent model.
+type PipelineEvent struct {
+	System   PipelineEventSystem `json:"system" yaml:"system"`
+	Reason   string              `json:"reason" yaml:"reason"`
+	Message  string              `json:"message" yaml:"message"`
+	LoggedAt time.Time           `json:"loggedAt" yaml:"loggedAt"`
+}
+
 // PipelineStatus model.
 type PipelineStatus struct {
 	ID        string             `json:"id" yaml:"id"`
 	Config    PipelineConfig     `json:"config" yaml:"config"`
 	Status    PipelineStatusKind `json:"status" yaml:"status"`
+	Events    []PipelineEvent    `json:"events,omitempty" yaml:"events,omitempty"`
 	CreatedAt time.Time          `json:"createdAt" yaml:"createdAt"`
 }
 
