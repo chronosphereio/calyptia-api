0ad29aca8f6c1b16d3894760b4aefe28f1e58bde
niedbalski <niedbalski@users.noreply.github.com>
2023-09-04 15:40:10 +0000

    [create-pull-request] automated change

--- types/errs/processing_rule_template.go
+++ types/errs/processing_rule_template.go
@@ -1,9 +1,11 @@
 package errs
 
 const (
-	ProcessingRuleTemplateAlreadyExists = ConflictError("processing rule template already exists")
-	ProcessingRuleTemplateNotFound      = NotFoundError("processing rule template not found")
-	InvalidProcessingRuleTemplateName   = InvalidArgumentError("invalid processing rule template name")
-	InvalidProcessingRuleDef            = InvalidArgumentError("invalid processing rule template definition")
-	InvalidProcessingRuleTemplateID     = InvalidArgumentError("invalid processing rule template ID")
+	ProcessingRuleTemplateAlreadyExists          = ConflictError("processing rule template already exists")
+	ProcessingRuleTemplateNotFound               = NotFoundError("processing rule template not found")
+	InvalidProcessingRuleTemplateName            = InvalidArgumentError("invalid processing rule template name")
+	InvalidProcessingRuleTemplateInput           = InvalidArgumentError("invalid processing rule template input")
+	InvalidProcessingRuleTemplatePipelineVersion = InvalidArgumentError("invalid processing rule template pipeline version")
+	InvalidProcessingRuleDef                     = InvalidArgumentError("invalid processing rule template definition")
+	InvalidProcessingRuleTemplateID              = InvalidArgumentError("invalid processing rule template ID")
 )
