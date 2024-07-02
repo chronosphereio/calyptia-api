ac2e06f75385d4645d0b43772812626d64447cce
calyptia-ci <98808777+calyptia-ci@users.noreply.github.com>
2024-07-02 16:20:36 +0100

    [create-pull-request] automated change (#295)
    Co-authored-by: patrick-stephens <6388272+patrick-stephens@users.noreply.github.com>

--- types/search.go
+++ types/search.go
@@ -9,15 +9,20 @@ type SearchQuery struct {
 type SearchResource string
 
 const (
-	SearchResourceCoreInstance    SearchResource = "core_instance"
-	SearchResourceResourceProfile SearchResource = "resource_profile"
-	SearchResourcePipeline        SearchResource = "pipeline"
-	SearchResourceFleet           SearchResource = "fleet"
 	SearchResourceAgent           SearchResource = "agent"
 	SearchResourceClusterObject   SearchResource = "cluster_object"
+	SearchResourceConfigSection   SearchResource = "config_section"
+	SearchResourceCoreInstance    SearchResource = "core_instance"
+	SearchResourceEnvironment     SearchResource = "environment"
+	SearchResourceFleet           SearchResource = "fleet"
+	SearchResourceMember          SearchResource = "member"
+	SearchResourcePipeline        SearchResource = "pipeline"
+	SearchResourcePipelineSecret  SearchResource = "pipeline_secret"
+	SearchResourceResourceProfile SearchResource = "resource_profile"
+	SearchResourceTraceSession    SearchResource = "trace_session"
 )
 
 type SearchResult struct {
-	ID   string
-	Name string
+	ID   string `json:"id" yaml:"id" db:"id"`
+	Name string `json:"name" yaml:"name" db:"name"`
 }
