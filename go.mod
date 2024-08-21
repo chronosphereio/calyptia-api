ffa7e09c79f4910b423e7397628d3fef11881f40
Nicolás Parada <nicolas@calyptia.com>
2024-08-21 10:47:12 -0400

    restore schemathesis (#300)
    * restore schemathesis
    
    * update go.mod deps

--- go.mod
+++ go.mod
@@ -1,17 +1,16 @@
 module github.com/calyptia/api
 
-go 1.22.4
+go 1.22.6
 
 require (
-	github.com/calyptia/go-fluentbit-config/v2 v2.3.1
+	github.com/calyptia/go-fluentbit-config/v2 v2.3.3
 	github.com/go-logfmt/logfmt v0.6.0
 	github.com/peterhellberg/link v1.2.0
 	github.com/vmihailenco/msgpack/v5 v5.4.1
-	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
 )
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -19,16 +19,16 @@ fi
 # Schema linting
 docker run --pull=always --rm -v "$SPEC_DIR/":/spec:ro redocly/openapi-cli lint /spec/open-api.yml
 
-# Skipping schemathesis temporarily due to it failing randomly.
-#
 # Schema validation against the cloud image
-# docker run --pull=always --rm --network=host -v "$SPEC_DIR/":/spec:ro \
-#     schemathesis/schemathesis:stable \
-#         run \
-#         --hypothesis-suppress-health-check=too_slow \
-#         --header "Authorization:" \
-#         --header "X-Project-Token:$TOKEN" \
-#         --stateful=links \
-#         --workers "$WORKER_COUNT" \
-#         /spec/open-api.yml \
-#         --base-url="$CLOUD_URL/" "$@"
+docker run --pull=always --rm --network=host -v "$SPEC_DIR/":/spec:ro \
+    schemathesis/schemathesis:stable run \
+        --base-url="$CLOUD_URL" \
+        --checks all \
+        --exclude-checks status_code_conformance \
+        --header "X-Project-Token: $TOKEN" \
+        --stateful=links \
+        --exclude-operation-id awsCustomerRedirect \
+        --contrib-openapi-formats-uuid \
+        --contrib-openapi-fill-missing-examples \
+        --workers "$WORKER_COUNT" \
+        /spec/open-api.yml "$@"
